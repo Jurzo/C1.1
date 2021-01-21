29265db4c80a0729a9e3363bc4303b12a4771805
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-01-21 15:44:32 +0200

    C1.1 Done

---
+++ hello/src/main/java/fi/hh/swd20/hello/web/HelloController.java
@@ -0,0 +1,28 @@
+package fi.hh.swd20.hello.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "This is the main page";
+    }
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "This is the contact page!";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam( name = "location" ) String loc,
+                        @RequestParam( name = "name" ) String name) {
+        return "Welcome to the " + loc + " " + name;
+    }
+    
+}
---
+++ hello/src/test/java/fi/hh/swd20/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.hh.swd20.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
